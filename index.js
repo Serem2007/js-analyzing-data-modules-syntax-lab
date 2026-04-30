c7ef8cade0f2f9941fabeb2dc616e28ee03e1c07
Serem2007 <levy.serem@student.moringaschool.com>
2026-04-30 15:46:06 +0300

    Completed Analyzing Data Modules

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+require('datejs');
+
+function combineUsers(...args) {
+  const combinedObject = {
+    users: []
+  };
+
+  for (let arr of args) {
+    combinedObject.users.push(...arr);
+  }
+
+  combinedObject.merge_date = new Date().toString('M/d/yyyy');
+
+  return combinedObject;
+}
 
 
 
